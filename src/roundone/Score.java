1331c1b3f8dc2917c519d151ab8786a7dfca7046
Michael Kramer <mpk5206@psu.edu>
2017-09-05 13:01:05 -0400

    Added Accessors and Mutators

--- src/roundone/Score.java
+++ src/roundone/Score.java
@@ -12,4 +12,32 @@ package roundone;
 class Score {
     private String name;
     private int points = 0;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the points
+     */
+    public int getPoints() {
+        return points;
+    }
+
+    /**
+     * @param points the points to set
+     */
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
