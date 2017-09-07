3928e4d9517e9bc921847e62da9949c3f84cd178
Nathan Smith <n.smith167@gmail.com>
2017-09-07 12:24:14 -0400

    Update MenuModel

--- src/roundone/MenuModel.java
+++ src/roundone/MenuModel.java
@@ -5,22 +5,102 @@
  */
 package roundone;
 
-import java.util.HashMap;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  *
- * @author zjx5013
+ * @author nps5120
  */
 public class MenuModel {
     private String name; //This is the current Username
-    private Score[] highScores = new Score[3];
+    private Score[] highScores;
     
-    public void buildGameFrame(GameView gView, GameModel gModel, GameController 
-            gController) {
+    public MenuModel()
+    {
+        highScores = new Score[3];
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public void addScore(String name, String score)
+    {
+        Score newScore = new Score(name, score);
+        
+        if(newScore.getPoints().compareTo(highScores[0].getPoints()) > 0)
+        {
+            highScores[2] = highScores[1];
+            highScores[1] = highScores[0];
+            highScores[0] = newScore;
+        }
+        else if(newScore.getPoints().compareTo(highScores[1].getPoints()) > 0)
+        {
+            highScores[2] = highScores[1];
+            highScores[1] = newScore;
+        }
+        else if(newScore.getPoints().compareTo(highScores[2].getPoints()) > 0)
+        {
+            highScores[2] = newScore;
+        }
+        
+        saveScores();
+    }
+    
+    private boolean saveScores()
+    {
+        try
+        {
+            PrintWriter writer = new PrintWriter("leaderboard.txt");
+            for(int i = 0; i < 3; i++)
+            {
+                writer.print(highScores[i].getName() + "\n");
+                writer.print(highScores[i].getPoints() + "\n");
+                
+            }
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+    }
+    
+    private boolean loadScores()
+    {
+        try
+        {
+            Scanner fileIn = new Scanner("leaderboard.txt");
+            fileIn.useDelimiter("\n");
+            for(int i = 0; i < 3; i++)
+            {
+                highScores[i].setName(fileIn.nextLine());
+                highScores[i].setPoints(fileIn.nextLine());
+            }
+            
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+    }
+    
+    public void buildGameFrame(GameView gView, GameModel gModel, GameController gController) 
+    {
         System.err.println("This is a stub.");
     }
     
     public Score[] getHighScores() {
         return this.highScores;
     }
+    
+    
 }
