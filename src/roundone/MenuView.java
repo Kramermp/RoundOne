3743272ef4ab3b8ee648c5f4427f3744d4a4605a
rbitt <rbitt@10.0.0.205>
2017-09-11 23:59:21 -0400

    Modified Menu View  - Added Leaderboards (Not yet functional) and Instructions.

--- src/roundone/MenuView.java
+++ src/roundone/MenuView.java
@@ -32,69 +32,173 @@ public class MenuView extends JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        startButton = new javax.swing.JButton();
-        nameField = new javax.swing.JTextField();
-        nameLabel = new javax.swing.JLabel();
+        startButton1 = new javax.swing.JButton();
+        nameField1 = new javax.swing.JTextField();
+        titleLabel = new javax.swing.JLabel();
+        nameLabel1 = new javax.swing.JLabel();
+        instructionsLabel = new javax.swing.JLabel();
+        i1Label = new javax.swing.JLabel();
+        i2Label = new javax.swing.JLabel();
+        i3Label = new javax.swing.JLabel();
+        leaderboardLabel = new javax.swing.JLabel();
+        lbName1Label = new javax.swing.JLabel();
+        lbPts1Label = new javax.swing.JLabel();
+        lbScore1Label = new javax.swing.JLabel();
+        lbName2Label = new javax.swing.JLabel();
+        lbPts2Label = new javax.swing.JLabel();
+        lbScore2Label = new javax.swing.JLabel();
+        lbName3Label = new javax.swing.JLabel();
+        lbPts3Label = new javax.swing.JLabel();
+        lbScore3Label = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        startButton.setText("Start Game");
-        startButton.addActionListener(new java.awt.event.ActionListener() {
+        startButton1.setText("Start Game");
+        startButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                startButtonActionPerformed(evt);
+                startButton1ActionPerformed(evt);
             }
         });
 
-        nameField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                nameFieldActionPerformed(evt);
-            }
-        });
+        titleLabel.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        titleLabel.setText("Munchers");
 
-        nameLabel.setText("Please enter your nickname here:");
+        nameLabel1.setText("Please enter your nickname here:");
+
+        instructionsLabel.setText("Instructions:");
+
+        i1Label.setText("The object of the game is to, out of all of the numbers on screen, select the prime numbers.");
+
+        i2Label.setText("Earn points by selecting prime numbers, lose points for selecting non-prime numbers.");
+
+        i3Label.setText("The game ends when all of the prime numbers have been selected.");
+
+        leaderboardLabel.setText("Leaderboard");
+
+        lbName1Label.setText("AAA");
+
+        lbPts1Label.setText("pts");
+
+        lbScore1Label.setText("0");
+
+        lbName2Label.setText("AAA");
+
+        lbPts2Label.setText("pts");
+
+        lbScore2Label.setText("0");
+
+        lbName3Label.setText("AAA");
+
+        lbPts3Label.setText("pts");
+
+        lbScore3Label.setText("0");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(52, Short.MAX_VALUE)
+                .addGap(68, 68, 68)
+                .addComponent(i3Label)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(i1Label)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(9, 9, 9)
+                                .addComponent(nameLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(nameField1, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lbName1Label, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(lbName2Label)
+                                    .addComponent(lbName3Label))
+                                .addGap(82, 82, 82)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                            .addComponent(lbScore3Label)
+                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                            .addComponent(lbPts3Label))
+                                        .addGroup(layout.createSequentialGroup()
+                                            .addComponent(lbScore2Label)
+                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                            .addComponent(lbPts2Label)))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(lbScore1Label)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(lbPts1Label)))
+                                .addGap(160, 160, 160))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(titleLabel)
+                                .addGap(169, 169, 169))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(startButton1)
+                                    .addComponent(i2Label))
+                                .addGap(27, 27, 27))))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(nameLabel)
-                        .addGap(18, 18, 18)
-                        .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(67, 67, 67))
+                        .addComponent(instructionsLabel)
+                        .addGap(196, 196, 196))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(startButton)
-                        .addGap(164, 164, 164))))
+                        .addComponent(leaderboardLabel)
+                        .addGap(197, 197, 197))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(94, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(7, 7, 7)
+                .addComponent(titleLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(instructionsLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(i1Label)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(i2Label)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(i3Label)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(nameLabel))
-                .addGap(87, 87, 87)
-                .addComponent(startButton)
-                .addGap(64, 64, 64))
+                    .addComponent(nameLabel1)
+                    .addComponent(nameField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(startButton1))
+                .addGap(18, 18, 18)
+                .addComponent(leaderboardLabel)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbScore1Label)
+                    .addComponent(lbPts1Label)
+                    .addComponent(lbName1Label))
+                .addGap(5, 5, 5)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbScore2Label)
+                    .addComponent(lbPts2Label)
+                    .addComponent(lbName2Label))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbName3Label)
+                    .addComponent(lbScore3Label)
+                    .addComponent(lbPts3Label))
+                .addGap(32, 32, 32))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void nameFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameFieldActionPerformed
+    private void startButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startButton1ActionPerformed
         // TODO add your handling code here:
-        
-        
-    }//GEN-LAST:event_nameFieldActionPerformed
-
-    private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startButtonActionPerformed
-        // TODO add your handling code here:
-        GameController theGameView=new GameController();
-        //theGameView.setVisible(true);
-    }//GEN-LAST:event_startButtonActionPerformed
+    }//GEN-LAST:event_startButton1ActionPerformed
     public String getUsername(){
     String username=this.nameField.getText();
     return username;
@@ -136,9 +240,24 @@ public class MenuView extends JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField nameField;
-    private javax.swing.JLabel nameLabel;
-    private javax.swing.JButton startButton;
+    private javax.swing.JLabel i1Label;
+    private javax.swing.JLabel i2Label;
+    private javax.swing.JLabel i3Label;
+    private javax.swing.JLabel instructionsLabel;
+    private javax.swing.JLabel lbName1Label;
+    private javax.swing.JLabel lbName2Label;
+    private javax.swing.JLabel lbName3Label;
+    private javax.swing.JLabel lbPts1Label;
+    private javax.swing.JLabel lbPts2Label;
+    private javax.swing.JLabel lbPts3Label;
+    private javax.swing.JLabel lbScore1Label;
+    private javax.swing.JLabel lbScore2Label;
+    private javax.swing.JLabel lbScore3Label;
+    private javax.swing.JLabel leaderboardLabel;
+    private javax.swing.JTextField nameField1;
+    private javax.swing.JLabel nameLabel1;
+    private javax.swing.JButton startButton1;
+    private javax.swing.JLabel titleLabel;
     // End of variables declaration//GEN-END:variables
 
     void populateLeaderboard(Score[] highScores) {
